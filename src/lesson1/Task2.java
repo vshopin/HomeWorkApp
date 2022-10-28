1216336b3438b413a27459b2f9247d9b4e8408de
shopi <shopin_va@mail.ru>
2022-10-28 21:29:56 +0700

    added four variables and added Scanner

--- src/lesson1/Task2.java
+++ src/lesson1/Task2.java
@@ -1,10 +1,18 @@
 package lesson1;
 
-public class Task2 {
-    public static void main(String[] args) {
-        System.out.println(returnExpression(4, 6, 8, 10));
-    }
+import java.util.Scanner;
 
+public class Task2 {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Введите 4 числа: ");
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int num3 = scanner.nextInt();
+        int num4 = scanner.nextInt();
+        System.out.println(returnExpression(num1, num2, num3, num4));
+    }
     private static int returnExpression(int a, int b, int c, int d) {
         return a * (b + (c / d));
     }
