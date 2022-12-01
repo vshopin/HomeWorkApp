f2cf4eceb95f6c22f9bece54baf71bfb5a174d9b
shopi <shopin_va@mail.ru>
2022-12-01 23:01:05 +0700

    created array of person's info and get person's info with class Person

---
+++ src/lesson5/ListPerson.java
@@ -0,0 +1,18 @@
+package lesson5;
+
+public class ListPerson {
+    public static void main(String[] args) {
+        Person[] personArray = new Person[5];
+        personArray[0] = new Person("Ivan", "Ivanov", "Developer", "ivanov@gmail.com", "79130000102", 200000, 42);
+        personArray[1] = new Person("Petr", "Petrov", "Developer", "petrov@gmail.com", "79130000103", 160000, 35);
+        personArray[2] = new Person("Semen", "Sidorov", "Administrator", "sidorov@gmail.com", "79130000104", 50000, 50);
+        personArray[3] = new Person("Dima", "Medvedev", "Devops", "medvedev@gmail.com", "79130000105", 150000, 35);
+        personArray[4] = new Person("Igor", "Zemlin", "QA", "zemlin@gmail.com", "79130000106", 30000, 25);
+
+        for (int i = 0; i < personArray.length; i++) {
+            if (personArray[i].age >= 40) {
+                personArray[i].getInfoPerson();
+            }
+        }
+    }
+}
