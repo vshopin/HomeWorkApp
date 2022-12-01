32c1a5cc4433e690c0e0dd3935eba772e6583c43
shopi <shopin_va@mail.ru>
2022-12-01 22:59:49 +0700

    created class Person with constructor of person's info created method for print person's info

---
+++ src/lesson5/Person.java
@@ -0,0 +1,26 @@
+package lesson5;
+
+public class Person {
+
+    String firstName;
+    String lastName;
+    String position;
+    String email;
+    String phone;
+    int salary;
+    int age;
+
+    Person(String firstName, String lastName, String position, String email, String phone, int salary, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+    public void getInfoPerson() {
+        System.out.println(this.firstName + "; " + this.lastName+ "; " + this.position + "; " + this.email + "; " + this.phone + "; " + this.salary + "; " + this.age);
+    }
+
+}
